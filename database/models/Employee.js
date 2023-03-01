78d94dc7cdcf12fa3d0755faedbfdcfa3271d8d2
Nattayatorn Techasith <keithnatt.techasith@gmail.com>
2023-03-01 18:43:45 -0500

    Database setup

---
+++ database/models/Employee.js
@@ -0,0 +1,23 @@
+
+
+const Sequelize = require('sequelize');
+const db = require('../db'); 
+
+const Employee = db.define("employee",{ 
+
+first_name:{
+    type: Sequelize.STRING,
+    allowNull: false
+},
+
+last_name: {
+    type: Sequelize.STRING,
+    allowNull: False
+}, 
+
+department: {
+    type: Sequelize.STRING, 
+}
+}); 
+
+module.exports = Employee; 

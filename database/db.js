0b17b4bf6ed321002e4f78405226f0a8751374c4
pablo122361 <lito23.pjp@gmail.com>
2023-03-27 16:39:53 -0400

    fixed small typos

--- database/db.js
+++ database/db.js
@@ -1,14 +1,13 @@
 //Instantiate database 
 
 //model dependencies 
-
 const Sequelize = require('sequelize')
 
+//entry point
 const db = new Sequelize('ems-backend', 'postgres', 'password',{
-    host: 'local host',
+    host: 'localhost',
     dialect: 'postgres'
 });
 
 //Export sequelize 
-
-module.exports = db; 
+module.exports = db;

e69c473db1eb5270079b41391dbaee467f9b5b18
pablo122361 <lito23.pjp@gmail.com>
2023-03-27 17:35:40 -0400

    dummy data

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -1,6 +1,32 @@
 const { Employee, Task } = require('../models');
 
 const seedDB = async () => {
+	const dummyEmployee = await Employee.create({
+		first_name: "John",
+		last_name: "Doe",
+		department: "Engineering"
+	});
+
+	const dummyEmployee1 = await Employee.create({
+		first_name: "Hello",
+		last_name: "Kitty",
+		department: "HR"
+	});
+
+	const dummyTask = await Task.create({
+		description: "Build space rocket",
+        priority_level: "Low",
+		completion_status: "Not Completed"
+	});
+
+	const dummyTask1 = await Task.create({
+		description: "Fire John",
+        priority_level: "Low",
+		completion_status: "Not Completed"
+	});
+
+	await dummyTask.setEmployee(dummyEmployee);
+	await dummyTask1.setEmployee(dummyEmployee1);
 	
 }
 

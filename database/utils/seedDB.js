1c602a2628f1d213a39105db080f8d16b7b7b850
Nattayatorn Techasith <keithnatt.techasith@gmail.com>
2023-05-08 17:09:00 -0400

    Updated dummy data

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -9,7 +9,7 @@ const seedDB = async () => {
 
 	const dummyEmployee1 = await Employee.create({
 		first_name: "Kevin",
-		last_name: "Space",
+		last_name: "Spacey",
 		department: "HR"
 	});
 
@@ -38,7 +38,7 @@ const seedDB = async () => {
     const dummyEmployee6 = await Employee.create({
         first_name: "Pablo",
         last_name: "Pena",
-        department: "Chief Financial Officer"
+        department: "Chief Technology Officer"
     });
 
     const dummyEmployee7 = await Employee.create({
@@ -91,88 +91,178 @@ const seedDB = async () => {
         comments: "Disputes with fellow coworkers",
     });
     const dummyTask7 = await Task.create({
-        description: "Give documents to Chairman",
+        description: "Chairman's Signature",
         completion_status: "Not Completed",
-        due_date: "2023/07/15",
-        comments: "needs chairman's signature",
+        due_date: "2023/07/14",
+        comments: "Project approval",
     });
     const dummyTask8 = await Task.create({
-        description: "Team meeting",
+        description: "Quarterly Meeting",
         completion_status: "Not Completed",
         due_date: "2023/07/20",
         comments: "Go over quarterly budgeting",
     });
     const dummyTask9 = await Task.create({
-        description: "Build a website",
+        description: "Meeting with the C.I.A.",
         completion_status: "Not Completed",
         due_date: "2023/07/25",
-        comments: "Webite about Fan sports content",
+        comments: "Software upgrades, Security upgrades",
     });
     const dummyTask10 = await Task.create({
-        description: "Build a website",
+        description: "Meeting with Client",
         completion_status: "Not Completed",
-        due_date: "2023/07/30",
-        comments: "Customer wants a website for dog adoption",
+        due_date: "2023/07/28",
+        comments: "Customer wants to improve product",
     });
 
 	const dummyOrder = await Order.create({
-		detail: "Cool looking rocket",
+		detail: "Build a rocket to mars",
 		request: "- Low cost \r\n- Cool Looking \r\n- Safe and secure \r\n",
 		customer: "NASA",
-		order_due_date: "2023/05/30"	
+		order_due_date: "2023/05/10"	
 	})
 	const dummyOrder1 = await Order.create({
 		detail: "New G5 plane",
 		request: "- Full options \r\n- Bullet Proof \r\n- Logo of the company on the side of plane",
 		customer: "Jeff Bezos",
-		order_due_date: "2023/07/25"	
+		order_due_date: "2023/05/15"	
 	})
 	const dummyOrder2 = await Order.create({
-		detail: "Customer wants to build a website, showing their company ",
+		detail: "Customer wants to build a website, ",
 		request: "- Clear goal \r\n- Technical prowess \r\n- Security \r\n- Great design",
-		customer: "StarBucks",
-		order_due_date: "2023/07/25"	
+		customer: "Amazon",
+		order_due_date: "2023/06/20"	
 	})
 	const dummyOrder3 = await Order.create({
 		detail: " Create and app aligning with company ",
 		request: "- Easy UI \r\n- 2 MFA Security \r\n- Great design",
 		customer: "StarBucks",
-		order_due_date: "2023/07/25"	
+		order_due_date: "2023/06/23"	
 	})
 	const dummyOrder4 = await Order.create({
-		detail: "Customer wants to build a website, showing their company ",
-		request: "- Clear goal \r\n- Technical prowess \r\n- Security \r\n- Great design",
-		customer: "StarBucks",
-		order_due_date: "2023/07/25"	
+		detail: "Customer wants to create an investing portfolio ",
+		request: "- Aggressive investing plan \r\n- Goal 10% annual profit \r\n- Energy Market",
+		customer: "David Villa",
+		order_due_date: "2023/06/30"	
 	})
+
+	const dummyOrder5 = await Order.create({
+		detail: "System upgrades",
+		request: "- Software upgrade, \r\n -Security upgrades \r\n -Modern design",
+		customer: "C.I.A",
+		order_due_date:"2023/7/25"
+	})
+
+
+	const dummyOrder6 = await Order.create({
+		detail: "Customer wants to improve product",
+		request: "- Easier UI, \r\n -Design improvements \r\n -Software updates",
+		customer: "CUNY",
+		order_due_date:"2023/7/28"
+	})
+
 	const dummyEvent = await Event.create({
 		title: "task1",
-		start_date: "2023/05/04",
-		end_date: "2023/05/17",
+		start_date: "2020/05/04",
+		end_date: "2023/05/10",
 	})
 	const dummyEvent1 = await Event.create({
 		title: "task2",
-		start_date: "2023/05/15",
-		end_date: "2023/05/20",
+		start_date: "2021/05/15",
+		end_date: "2023/05/15",
 	})
+
+	const dummyEvent2 = await Event.create({
+		title: "task3",
+		start_date: "2023/01/15",
+		end_date: "2023/06/20",
+	})
+
+	const dummyEvent3 = await Event.create({
+		title: "task4",
+		start_date: "2023/02/01",
+		end_date: "2023/06/23",
+	})
+
+	const dummyEvent4 = await Event.create({
+		title: "task5",
+		start_date: "2023/06/19",
+		end_date: "2023/06/23",
+	})
+
+	const dummyEvent5 = await Event.create({
+		title: "task6",
+		start_date: "2023/07/02",
+		end_date: "2023/07/05",
+	})
+
+	const dummyEvent6 = await Event.create({
+		title: "task7",
+		start_date: "2023/07/07",
+		end_date: "2023/07/10",
+	})
+
+	const dummyEvent7 = await Event.create({
+		title: "task8",
+		start_date: "2023/07/14",
+		end_date: "2023/07/14",
+	})
+
+	const dummyEvent8 = await Event.create({
+		title: "task9",
+		start_date: "2023/07/17",
+		end_date: "2023/07/20",
+	})
+
+	const dummyEvent9 = await Event.create({
+		title: "task10",
+		start_date: "2022/12/05",
+		end_date: "2023/07/25",
+	})
+
+	const dummyEvent10 = await Event.create({
+		title: "task11",
+		start_date: "2022/12/05",
+		end_date: "2023/07/28",
+	})
+
+
+
+	
+
+	// set tasks to employees 
 	await dummyTask.setEmployee(dummyEmployee);
-	await dummyTask1.setEmployee(dummyEmployee1);
+	await dummyTask1.setEmployee(dummyEmployee);
 	await dummyTask2.setEmployee(dummyEmployee2);
-	await dummyTask3.setEmployee(dummyEmployee3);
+	await dummyTask3.setEmployee(dummyEmployee2);
 	await dummyTask4.setEmployee(dummyEmployee4);
 	await dummyTask5.setEmployee(dummyEmployee5);
-	await dummyTask6.setEmployee(dummyEmployee6);
+	await dummyTask6.setEmployee(dummyEmployee1);
 	await dummyTask7.setEmployee(dummyEmployee7);
-	await dummyTask8.setEmployee(dummyEmployee7);
-	await dummyTask9.setEmployee(dummyEmployee7);
-	await dummyTask10.setEmployee(dummyEmployee2);
+	await dummyTask8.setEmployee(dummyEmployee3);
+	await dummyTask9.setEmployee(dummyEmployee6);
+	await dummyTask10.setEmployee(dummyEmployee6);
+
+//set orders on order's page 
 	await dummyTask.addOrder(dummyOrder);
 	await dummyTask2.addOrder(dummyOrder1);
 	await dummyTask2.addOrder(dummyOrder2);
 	await dummyTask3.addOrder(dummyOrder3);
 	await dummyTask4.addOrder(dummyOrder4);
+	await dummyTask5.addOrder(dummyOrder5); 
+
+//set calander with tasks. 
 	await dummyEvent;
 	await dummyEvent1;
+	await dummyEvent2;
+	await dummyEvent3;
+	await dummyEvent4;
+	await dummyEvent5;
+	await dummyEvent6;
+	await dummyEvent7;
+	await dummyEvent8;
+	await dummyEvent9;
+	await dummyEvent10;
 }
 
 module.exports = seedDB;
